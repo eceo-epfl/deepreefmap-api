0c5b6024491affa7533260fde701ddef9dec3e9c
Evan Thomas <evan.thomas@epfl.ch>
2024-04-25 10:56:12 +0200

    Get runai executable at startup

--- prestart.sh
+++ prestart.sh
@@ -1,3 +1,8 @@
+# Get RunAI executable (only possible on EPFL network)
+wget --content-disposition https://rcp-caas-test.rcp.epfl.ch/cli/linux
+chmod +x runai
+mv runai /usr/local/bin
+
 # Run migrations
 poetry run alembic upgrade head
 mkdir -p /app/.kube && cp /root/.kube/config.yaml /app/.kube/config.yaml
