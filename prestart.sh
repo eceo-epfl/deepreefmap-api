2059d51a591df41a83fb165db04cbb7102646384
Evan Thomas <evan@evanjt.com>
2026-08-19 18:01:53 +0200

    Remove FastAPI implementation

--- prestart.sh
+++
@@ -1,4 +0,0 @@
-# Run migrations
-poetry run alembic upgrade head
-mkdir -p /app/.kube && cp /root/.kube/config.yaml /app/.kube/config.yaml
-uvicorn --host=0.0.0.0 --timeout-keep-alive=0 app.main:app --reload
